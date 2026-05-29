d6e7fc6c6faf5e692908b00b69fb948fb773a7b6
tiengalaxy <tiengalaxy@users.noreply.github.com>
2026-05-29 17:38:19 +0000

    chore: bump version to 1.14.0

--- scripts/ch_lib/util.py
+++ scripts/ch_lib/util.py
@@ -6,7 +6,7 @@ import requests
 import shutil
 
 
-version = "1.13.0"
+version = "1.14.0"
 
 def_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                "Authorization": ""}
